5a4263217da3e1cd1efd641fdcc318a849dda7ac
Beanalini <dat826@gmail.com>
2022-10-10 12:54:11 +0100

    moved css and  images, edited homepage to include body html only and added a links to register page. Added a temporary register page for testing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,30 @@
 const router = require('express').Router();
 
+//serve homepage
+router.get('/', async (req, res) => {
+    try {
+      
+      res.render('homepage');
+  
+     
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+  router.get('/register', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/register');
+      return;
+    }
+  
+    res.render('register');
+  });
+
+  
+
 //To Do: any data that needs to be rendered for stats
 
 
